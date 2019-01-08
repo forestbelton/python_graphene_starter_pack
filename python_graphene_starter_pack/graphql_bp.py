6b7599294694b647051c9eee3794806deea1531a
Forest Belton <forest@homolo.gy>
2019-01-07 23:40:22 -0800

    Execute GraphQL query

--- python_graphene_starter_pack/graphql_bp.py
+++ python_graphene_starter_pack/graphql_bp.py
@@ -1,7 +1,29 @@
-from flask import Blueprint, request
+import json
+from flask import Blueprint, request, Response
+from werkzeug.exceptions import BadRequest
+
+from .schema import schema
 
 graphql_bp = Blueprint('graphql', __name__)
 
-@graphql_bp.route('/')
+@graphql_bp.route('/', methods=['POST'])
 def handle_graphql_request():
-    return 'OK'
+    data = request.get_json() or {}
+
+    if 'query' in data and 'variables' in data:
+        query = data['query']
+        variables = data['variables']
+
+        output = schema.execute(query, variables=variables)
+        errors = output.errors or []
+        result = {
+            'data': output.data,
+            'errors': [e.message for e in errors],
+        }
+    else:
+        result = {
+            'data': None,
+            'errors': ['Malformed input query.'],
+        }
+
+    return Response(json.dumps(result), mimetype='application/json')
